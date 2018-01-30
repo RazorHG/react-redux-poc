dabb63378b74eac839b2bae724c7315e17217196
Kevin Barcelon <kbarcelon@nexient.com>
2018-01-30 14:40:38 -0500

    setup babel and dev server

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,17 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+      rules: [
+          { 
+              test: /\.jsx?$/, 
+              loader: 'babel-loader', 
+              exclude: /node_modules/ 
+          }
+      ]
+  },
+  devServer: {
+      contentBase: path.resolve(__dirname, "dist")
   }
 };
